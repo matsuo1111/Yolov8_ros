23178f2ef13feee3d5a1bfe3729805ccfc6bb305
yoshiko-satoken <kulala.mbili@gmail.com>
2024-04-16 10:19:36 +0900

    add multiple_weight

--- yolov8_ros/scripts/yolo_v8.py
+++ yolov8_ros/scripts/yolo_v8.py
@@ -13,90 +13,72 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from yolov8_ros_msgs.msg import BoundingBox, BoundingBoxes
 
-
 class Yolo_Dect:
     def __init__(self):
-
-        # load parameters
+        # Load parameters
         weight_path = rospy.get_param('~weight_path', '')
-        image_topic = rospy.get_param(
-            '~image_topic', '/camera/color/image_raw')
+        image_topic = rospy.get_param('~image_topic', '/camera/color/image_raw')
         pub_topic = rospy.get_param('~pub_topic', '/yolov8/BoundingBoxes')
         self.camera_frame = rospy.get_param('~camera_frame', '')
-        conf = rospy.get_param('~conf', '0.5')
-        self.visualize = rospy.get_param('~visualize', 'True')
+        conf = rospy.get_param('~conf', 0.5)
+        self.visualize = rospy.get_param('~visualize', True)
 
-        # which device will be used
-        if (rospy.get_param('/use_cpu', 'false')):
-            self.device = 'cpu'
-        else:
-            self.device = 'cuda'
+        # Device configuration
+        self.device = 'cpu' if rospy.get_param('/use_cpu', False) else 'cuda'
 
-#        self.model = YOLO(os.path.join(weight_path, 'yolov8s.pt'))
-        self.model = YOLO(os.path.join(weight_path, 'yolov8n.pt'))
-        self.model.fuse()
+        # Load models
+        self.model1 = YOLO(os.path.join(weight_path, 'yolov8s.pt'))
+        self.model2 = YOLO(os.path.join(weight_path, 'yolov8m.pt'))
+        self.model1.fuse()
+        self.model2.fuse()
 
-        self.model.conf = conf
-        self.color_image = Image()
-        self.getImageStatus = False
+        self.model1.to(self.device)
+        self.model2.to(self.device)
+        self.model1.conf = conf
+        self.model2.conf = conf
 
-        # Load class color
-        self.classes_colors = {}
+        # Image subscription
+        self.color_sub = rospy.Subscriber(image_topic, Image, self.image_callback, queue_size=1, buff_size=52428800)
 
-        # image subscribe
-        self.color_sub = rospy.Subscriber(image_topic, Image, self.image_callback,
-                                          queue_size=1, buff_size=52428800)
-
-        # output publishers
-        self.position_pub = rospy.Publisher(
-            pub_topic,  BoundingBoxes, queue_size=1)
-
-        self.image_pub = rospy.Publisher(
-            '/yolov8/detection_image',  Image, queue_size=1)
-
-        # if no image messages
-        while (not self.getImageStatus):
-            rospy.loginfo("waiting for image.")
-            rospy.sleep(2)
+        # Output publishers
+        self.position_pub = rospy.Publisher(pub_topic, BoundingBoxes, queue_size=1)
+        self.image_pub = rospy.Publisher('/yolov8/detection_image', Image, queue_size=1)
 
     def image_callback(self, image):
-
-        self.boundingBoxes = BoundingBoxes()
-        self.boundingBoxes.header = image.header
-        self.boundingBoxes.image_header = image.header
-        self.getImageStatus = True
-        self.color_image = np.frombuffer(image.data, dtype=np.uint8).reshape(
-            image.height, image.width, -1)
-
+        self.color_image = np.frombuffer(image.data, dtype=np.uint8).reshape(image.height, image.width, -1)
         self.color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB)
+        
+        # Process image with both models
+        results1 = self.model1(self.color_image, show=False, conf=0.3)
+        results2 = self.model2(self.color_image, show=False, conf=0.3)
 
-        results = self.model(self.color_image, show=False, conf=0.3)
+        # Combined frame for visualization
+        combined_frame = self.color_image.copy()  # Use a copy of the original frame for drawing
 
-        self.dectshow(results, image.height, image.width)
+        # Handle and publish results
+        combined_frame = self.handle_results(results1, combined_frame)
+        combined_frame = self.handle_results(results2, combined_frame)
 
-        cv2.waitKey(3)
+        self.publish_image(combined_frame, image.height, image.width)
 
-    def dectshow(self, results, height, width):
+        if self.visualize:
+            cv2.imshow('YOLOv8 Combined Detection', combined_frame)
+            cv2.waitKey(1)
 
-        self.frame = results[0].plot()
-        print(str(results[0].speed['inference']))
-        fps = 1000.0/ results[0].speed['inference']
-        cv2.putText(self.frame, f'FPS: {int(fps)}', (20,50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
-
+    def handle_results(self, results, frame):
         for result in results[0].boxes:
             boundingBox = BoundingBox()
-            boundingBox.xmin = np.int64(result.xyxy[0][0].item())
-            boundingBox.ymin = np.int64(result.xyxy[0][1].item())
-            boundingBox.xmax = np.int64(result.xyxy[0][2].item())
-            boundingBox.ymax = np.int64(result.xyxy[0][3].item())
+            boundingBox.xmin = int(result.xyxy[0][0].item())
+            boundingBox.ymin = int(result.xyxy[0][1].item())
+            boundingBox.xmax = int(result.xyxy[0][2].item())
+            boundingBox.ymax = int(result.xyxy[0][3].item())
             boundingBox.Class = results[0].names[result.cls.item()]
             boundingBox.probability = result.conf.item()
-            self.boundingBoxes.bounding_boxes.append(boundingBox)
-        self.position_pub.publish(self.boundingBoxes)
-        self.publish_image(self.frame, height, width)
 
-        if self.visualize :
-            cv2.imshow('YOLOv8', self.frame)
+            # Draw bounding box on the frame
+            cv2.rectangle(frame, (boundingBox.xmin, boundingBox.ymin), (boundingBox.xmax, boundingBox.ymax), (0, 255, 0), 2)
+            cv2.putText(frame, f'{boundingBox.Class}: {boundingBox.probability:.2f}', (boundingBox.xmin, boundingBox.ymin-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        return frame
 
     def publish_image(self, imgdata, height, width):
         image_temp = Image()
@@ -104,19 +86,16 @@ class Yolo_Dect:
         header.frame_id = self.camera_frame
         image_temp.height = height
         image_temp.width = width
-        image_temp.encoding = 'bgr8'
+        image_temp.encoding = 'rgb8'
         image_temp.data = np.array(imgdata).tobytes()
         image_temp.header = header
         image_temp.step = width * 3
         self.image_pub.publish(image_temp)
-
 
 def main():
     rospy.init_node('yolov8_ros', anonymous=True)
     yolo_dect = Yolo_Dect()
     rospy.spin()
 
-
 if __name__ == "__main__":
-
     main()
